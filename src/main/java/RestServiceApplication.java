542819e6268cf72e09b45007d9672aee231092fb
oram1288 <cody.oram@keyin.com>
2024-10-21 19:26:14 -0230

    commit

--- src/main/java/RestServiceApplication.java
+++ src/main/java/RestServiceApplication.java
@@ -1,5 +1,3 @@
-package com.keyin;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
