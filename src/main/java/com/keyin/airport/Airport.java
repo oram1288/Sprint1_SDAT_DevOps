ba644e56dab035770397355dd5f5e5c6ded2ed2f
oram1288 <cody.oram@keyin.com>
2024-10-28 14:17:40 -0230

    airport commit - Reagan

---
+++ src/main/java/com/keyin/airport/Airport.java
@@ -0,0 +1,84 @@
+package com.keyin.airport;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private City city;
+
+    @ManyToMany(mappedBy = "airports")
+    private List<Aircraft> aircrafts = new ArrayList<>();
+
+    // Constructors
+    public Airport() {}
+
+    public Airport(String name, String code, City city) {
+        this.name = name;
+        this.code = code;
+        this.city = city;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    public List<Aircraft> getAircrafts() {
+        return aircrafts;
+    }
+
+    public void setAircrafts(List<Aircraft> aircrafts) {
+        this.aircrafts = aircrafts;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", code='" + code + '\'' +
+                ", city=" + city.getName() +
+                '}';
+    }
+}
