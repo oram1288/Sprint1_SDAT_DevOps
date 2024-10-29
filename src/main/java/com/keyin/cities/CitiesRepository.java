d5aa1bc5178977f5b97c9f8519588bfc5958caf8
oram1288 <cody.oram@keyin.com>
2024-10-29 15:28:27 -0230

    Repo-Service-Controller commit

--- src/main/java/com/keyin/cities/CitiesRepository.java
+++ src/main/java/com/keyin/cities/CitiesRepository.java
@@ -3,5 +3,5 @@ package com.keyin.cities;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CitiesRepository extends CrudRepository<Cities, Long> {
-    Iterable<Cities> findCitiesByAirportName(String airports_airportsName);
+    Iterable<Cities> findCityByAirportName(String airport_name);
 }
